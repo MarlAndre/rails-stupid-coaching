323c3f8e49764cb3f8015aba1d4fa6884f5c3322
Marlene Andre <andre.marlene1990@gmail.com>
2022-04-05 20:38:27 -0400

    interface is working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,23 +4,17 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = coach_answer(@question)
     @question = params[:question]
 
-  end
-
-  private
-
-  def coach_answer(question)
-    # TODO: return coach answer to question
     good_answer = "I am going to work right now!"
 
-    if question.end_with?("?")
-      return "Silly question, get dressed and go to work!"
-    elsif question.upcase != good_answer.upcase
-      return "I don't care, get dressed and go to work!"
+    if @question.end_with?("?")
+      @answer =  "Silly question, get dressed and go to work!"
+    elsif @question.upcase != good_answer.upcase
+      @answer = "I don't care, get dressed and go to work!"
     else
       return ""
     end
+
   end
 end
